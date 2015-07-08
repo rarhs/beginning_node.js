23712497671ba43ec18f7420dcfda97cdbeecafd
rarhs <reza054@gmail.com>
2015-07-09 03:11:41 +0600

    strange behavior

--- ch_3/intro/base/foo.js
+++ ch_3/intro/base/foo.js
@@ -1,3 +1,11 @@
 module.exports = function () {
     console.log('a function in file foo');
 };
+
+module.exports = function () {
+    console.log('My name is X');
+};
+
+module.exports = function namedFunction() {
+    console.log('My name is Y');
+};
